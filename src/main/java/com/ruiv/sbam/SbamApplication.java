0dd87109513ed9b4efec7f9913e4824f2396ad25
Rustam <Ivanyakov@netcracker.com>
2019-09-22 17:24:06 +0400

    single spring boot app

---
+++ src/main/java/com/ruiv/sbam/SbamApplication.java
@@ -0,0 +1,13 @@
+package com.ruiv.sbam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbamApplication.class, args);
+	}
+
+}
